0483b48a6362733ba8f64c6c772320e38e433d90
davidbp <macpro@macpro.local>
2017-11-05 23:18:10 +0100

    basic image processing with Images, tutorial pedestrians not working, url not valid

---
+++ Images/tutorial/get_data.bash
@@ -0,0 +1,14 @@
+wget http://cbcl.mit.edu/projects/cbcl/software-datasets/pedestrians128x64.tar.gz
+tar -xvzf pedestrians128x64.tar.gz
+rm pedestrians128x64.tar.gz
+mv -T pedestrians128x64 humans
+
+wget https://www.cis.upenn.edu/~jshi/ped_html/PennFudanPed.zip
+unzip PennFudanPed.zip
+rm PennFudanPed.zip
+
+mkdir not-humans
+julia preprocess_data.jl
+
+wget http://juliaimages.github.io/ImageFeatures.jl/latest/img/humans.jpg
+
