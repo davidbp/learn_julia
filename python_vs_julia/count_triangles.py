3c1a44e06f130b545d4635e048ac82ef67a47ddd
David Buchaca <davidbuchaca@gmail.com>
2018-06-28 15:12:13 +0200

    adding graph generator

--- python_vs_julia/count_triangles.py
+++ python_vs_julia/count_triangles.py
@@ -1,5 +1,5 @@
 
-import networkx
+import json 
 
 #### function for counting undirected cycles
 def generate_triangles(nodes):
@@ -26,14 +26,48 @@ def generate_triangles(nodes):
         visited_ids.add(node_a_id)
 
 
-node_number   = 100
-initial_nodes = 2
-G = networkx.barabasi_albert_graph(node_number, initial_nodes, seed=1) 
+#### function for counting undirected cycles
+def generate_triangles_2(nodes):
+    result = []
+    visited_ids = set() # mark visited node
+    for node_a_id in nodes:
+        temp_visited = set() # to get undirected triangles
+        for node_b_id in nodes[node_a_id]:
+            if node_b_id == node_a_id:
+                # to prevent self-loops, if your graph allows self-loops then you don't need this condition
+                raise ValueError 
+            if node_b_id in visited_ids:
+                continue
+            for node_c_id in nodes[node_b_id]:
+                if node_c_id in visited_ids:
+                    continue    
+                if node_c_id in temp_visited:
+                    continue
+                if node_a_id in nodes[node_c_id]:
+                    result.append((node_a_id, node_b_id, node_c_id))
+                else:
+                    continue
+            temp_visited.add(node_b_id)
+        visited_ids.add(node_a_id)
+        
+    return result
 
-adj_list = G.adjacency_list() 
-n_nodes  = len(adj_list)
-adj_dict = {i:adjacent_i for i,adjacent_i in zip(range(n_nodes), adj_list)}
+with open('graph.json', 'r') as fp:
+    adj_dict = json.load( fp)
 
-cycles = generate_triangles(adj_dict)
+adj_dict_ = {}
+for key in adj_dict:
+    adj_dict_[int(key)] =  adj_dict[key]
+del adj_dict
+
+
+import time
+
+t0 = time.time()
+print("Start Computing")
+cycles = generate_triangles(adj_dict_)
 cycles = list(cycles)
-print(cycles)
+print("Number of triangles: {}".format(len(cycles)))
+print( "Total time: {} seconds".format(abs(t0- time.time()))) 
+
+
